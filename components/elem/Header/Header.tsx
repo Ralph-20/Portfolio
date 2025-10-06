a7ee746e08d127e9631d35a98dc9fc4f57aaa342
Lucas <lucasjamesralph@gmail.com>
2025-10-06 18:35:39 -0500

    feat: adding header imaeg

--- components/elem/Header/Header.tsx
+++ components/elem/Header/Header.tsx
@@ -1,6 +1,8 @@
 import styles from './Header.module.scss';
 import { Tlink } from '@/types';
 import NavTab from '../NavTab';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export type HeaderProps = {
   // logo: ImageData;
@@ -10,6 +12,16 @@ export type HeaderProps = {
 const Header = ({ tabs }: HeaderProps): JSX.Element => {
   return (
     <div className={styles.main}>
+      <Link href="#" className={styles['logo-container']}>
+        <Image
+          src="/images/LJRDev.png"
+          alt="LJR Dev Logo"
+          width={180}
+          height={45}
+          priority
+          className={styles.logo}
+        />
+      </Link>
       <span className={styles['tabs-container']}>
         {tabs.map((tab, index) => (
           <NavTab key={index} tab={tab} />
